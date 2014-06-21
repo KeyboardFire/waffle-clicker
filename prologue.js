5a007ef09453b1ee73891cb1d13ddd3910879b42
KeyboardFire <andy@keyboardfire.com>
2014-06-21 10:37:53 -0500

    started exposition

--- prologue.js
+++ prologue.js
@@ -6,4 +6,13 @@ function prologue(onFinish) {
 
 function prologue_research(onFinish) {
 	$('#intro').remove();
+	$(document.body).append($('<div>').attr('id', 'discover')
+		.append($('<p>').text('In a series of generic plot developments, you have an forboding yet enigmatic vision, trip over a conveniently placed rock, and find a scroll that happens to say "How to make waffles" on the ground.'))
+		.append($('<button>').text('Read the scroll').click(function(){ prologue_read(onFinish); })));
+}
+
+function prologue_read(onFinish) {
+	$('#discover').remove();
+	$(document.body).append($('<div>').attr('id', 'scroll')
+		.append());
 }
